97ab1b43b1068d68a335fc4d31204a6ff629ce02
Gleb Zinoviev <gz0609@gmail.com>
2020-02-26 22:34:11 +0300

    feat: add solutions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array == null || array == undefined || array.length == 0){
+    return 0;
+  }
+  var b = array[0];
+  for ( var i = 0; i < array.length; i++){
+    if ( b > array[i]){
+      b = array[i];
+    }
+  } return b;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array == null || array == undefined || array.length == 0){
+    return 0;
+  }
+  var b = array[0];
+  for ( var i = 0; i < array.length; i++){
+    if ( b < array[i]){
+      b = array[i];
+    }
+  } return b;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array == null || array == undefined || array.length == 0){
+    return 0;
+  }
+  var sum = 0;
+  for ( var i = 0; i < array.length; i++){
+    sum += array[i];
+  }
+
+  return (sum/array.length);
 }
